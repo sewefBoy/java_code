343dfdadb9ff84d9d21f35b863ca059e20452e70
houyuxin <houyx0706@thundersoft.com>
2020-10-13 10:07:40 +0800

    update

---
+++ src/main/java/data_structure/SelectSort1.java
@@ -0,0 +1,34 @@
+package data_structure;
+
+/**
+ * 选择排序  时间复杂度 n方  不稳
+ */
+public class SelectSort1 {
+    public static void sort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int minPos = i;
+            for(int j=i+1;j<arr.length;j++){
+               if(arr[j] < arr[minPos]){
+                    minPos = j;
+               }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[minPos];
+            arr[minPos] = tmp;
+            System.out.println(arrtoString(arr));
+        }
+    }
+
+    public static String arrtoString(int arr[]) {
+        StringBuilder sb = new StringBuilder();
+        for(int i : arr) {
+            sb.append(i+ "  ");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,9,7,5,3,1};
+        sort(arr);
+    }
+}
